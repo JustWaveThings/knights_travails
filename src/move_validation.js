b975532e38742190d689fcd4e1d97a58770fc0d5
JustWaveThings <justwavethings@proton.me>
2023-03-28 11:26:49 -0400

    Move scope of actualValidMoves to inside function to prevent unwanted accumulation

--- src/move_validation.js
+++ src/move_validation.js
@@ -1,7 +1,5 @@
 import linkedListValidMovesInNumberType from './valid_moves_linked_list';
 
-const actualValidMoves = [];
-
 const allMovesTaken = [];
 
 export default function addValidMoves(movesArray, currentRoot) {
@@ -10,7 +8,7 @@ export default function addValidMoves(movesArray, currentRoot) {
 	if (!allMovesTaken.includes(currentRoot)) {
 		allMovesTaken.push(currentRoot);
 	}
-
+	const actualValidMoves = [];
 	movesArray.forEach(move => {
 		const moveInNumberFormat = Number(move);
 		if (
